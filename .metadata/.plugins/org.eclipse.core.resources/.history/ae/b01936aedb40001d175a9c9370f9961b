085ed9ef6d5188853baf86e047856c850d09c570
mestrehexed <sergiohenriquemaciel@gmail.com>
2022-10-06 14:20:58 -0300

    projeto limpo

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ae/b01936aedb40001d175a9c9370f9961b
@@ -0,0 +1,45 @@
+package com.Geraldo.bookstore.dtos;
+
+import java.io.Serializable;
+
+import com.Geraldo.bookstore.domain.Livro;
+
+public class LivroDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String titulo;
+	
+	// TODO Construtores
+	
+	public LivroDTO() {
+		super();
+	}
+
+	public LivroDTO( Livro obj) {
+		super();
+		this.id = obj.getId();
+		this.titulo = obj.getTitulo();
+	}
+	
+	// TODO Gets and Sets
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	
+}
---
+++ src/main/java/com/sergio/bookstore/dtos/CategoriaDTO.java
@@ -0,0 +1,65 @@
+package com.sergio.bookstore.dtos;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.sergio.bookstore.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty(message = "Campo NOME é requerido")
+	@Length(min = 3, max = 100, message = "O campo NOME deve ter entre 3 e 100 caracteres")
+	private String nome;
+	
+	@NotEmpty(message = "Campo DESCRICAO é requerido")
+	@Length(min = 3, max = 200, message = "O campo DESCRICAO deve ter entre 3 e 200 caracteres")
+	private String descricao;
+	
+	// TODO construtores
+	
+	public CategoriaDTO() {
+		super();
+	}
+
+	public CategoriaDTO( Categoria obj ) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.descricao = obj.getDescricao();
+	}
+
+	//TODO GETS and SETS 
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+
+}
